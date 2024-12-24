13f4921fa316e0d9ad7fdc13d4d79ba7afa7bf59
Tatyana <tatiana.filippova.98@mail.ru>
2024-12-24 14:30:08 +0300

    добавила header

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,50 @@
+import {
+  HeaderStyled,
+  Title,
+  Wrapper,
+  Name,
+  ImgVector,
+  HeaderBottom,
+  BreadCrumbsItem,
+} from "./styled";
+import localPost from "./assets/LocalPost.svg";
+import person from "./assets/Person.svg";
+import notifications from "./assets/Notifications.svg";
+import HeaderItem from "./HeaderItem";
+import vector from "./assets/Vector.svg";
+
+
+interface HeaderProps {
+  breadCrumbs: {
+    title: string;
+  }[]
+}
+
+const Header = ({breadCrumbs}: HeaderProps) => {
+  return (
+    <>
+      <HeaderStyled>
+        <Title>Список заказов</Title>
+        <Wrapper>
+          <HeaderItem icon={notifications.src} />
+          <HeaderItem icon={localPost.src} />
+          <HeaderItem icon={person.src} />
+          <Name>Игорь Красников</Name>
+          <ImgVector src={vector.src} />
+        </Wrapper>
+      </HeaderStyled>
+      <HeaderBottom>
+        {breadCrumbs.map((item, key) => {
+          return (
+            <BreadCrumbsItem key={item.title}>
+              {key > 0 && "  / "}
+              {item.title}
+            </BreadCrumbsItem>
+          );
+        })}
+      </HeaderBottom>
+    </>
+  );
+};
+
+export default Header;
