b2dae5f21ac4e0a12acd93a809ac8b9d17fe3c00
Tatyana <filippovat42@gmail.com>
2025-01-06 14:08:21 +0300

    test

---
+++ src/components/select/SelectInput/styled.ts
@@ -0,0 +1,33 @@
+import styled, { css } from "styled-components";
+
+interface InputProps {
+  $isErrors?: boolean;
+}
+
+export const Input = styled.input<InputProps>`
+  font-size: 16px;
+  border-bottom: 2px solid #9d9a9a;
+  padding: 12px 8px 12px 8px;
+  color: black;
+
+  &::placeholder {
+    color: black;
+  }
+
+  &:focus {
+    border-color: #000000;
+    outline: none;
+
+    ${(props) =>
+      props.$isErrors &&
+      css`
+        border-color: #ff0000;
+      `};
+  }
+
+  ${(props) =>
+    props.$isErrors &&
+    css`
+      border-color: #ff0000;
+    `}
+`;
